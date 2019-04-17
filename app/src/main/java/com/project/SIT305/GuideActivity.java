d99f05e28425c04ec8b2bbb8fc4776e0f786ec97
Your Name <you@example.com>
2019-04-17 08:56:14 +0800

    add GuideActivity,get familiar with Baidu Map api

---
+++ app/src/main/java/com/project/SIT305/GuideActivity.java
@@ -0,0 +1,17 @@
+package com.project.SIT305;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * DESC:
+ * Date: 2019/4/17  8:46 AM
+ * Time: 下午4:29
+ * author: liang
+ */
+public class GuideActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
